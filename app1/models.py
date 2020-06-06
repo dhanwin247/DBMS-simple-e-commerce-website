9e08eab3bd10a855ca883706e72eb8c6db7e4991
varun-raghavendra <varun.raghavendra1official@gmail.com>
2020-06-07 00:36:42 +0530

    add signup page

--- app1/models.py
+++ app1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=200, unique=True)
-    userid = models.IntegerField()
+    # userid = models.IntegerField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms
+from app1.models import User
 
 # Create your views here.
 def home(request):
@@ -14,6 +15,26 @@ def about(request):
 def signup(request):
     form = forms.SignupForm()
     if request.method == 'POST':
+        curr_user = User()
         form = forms.SignupForm(request.POST)
         #
+
     return render(request,'app1/signup.html',{'form':form})
+
+def signup_new(request):
+    if request.method == 'POST':
+        new_user = User()
+        new_user.username = request.POST.get('username')
+        new_user.password = request.POST.get('user_password')
+        new_user.email = request.POST.get('user_email')
+        new_user.first_name = request.POST.get('first_name')
+        new_user.last_name = request.POST.get('last_name')
+        new_user.delivery_address = request.POST.get('delivery_address')
+        new_user.phone_number = request.POST.get('phone_number')
+
+        # print(new_user)
+
+        new_user.save()
+
+    return render(request, 'app1/signup_new.html')
+
