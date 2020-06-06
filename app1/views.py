f59dafbeb75f51bb3a04f35d3eb8e366a08f46f5
Dhanwin Rao <dhanwinrao18@gmail.com>
2020-06-06 22:23:48 +0530

    Update views.py to reflect folder changes by Varun

--- app1/views.py
+++ app1/views.py
@@ -3,13 +3,13 @@ from . import forms
 
 # Create your views here.
 def home(request):
-    return render(request,'app1/home.html')
+    return render(request,'home/home.html')
 
 def login(request):
     return render(request,'app1/login.html')
 
 def about(request):
-    return render(request,'app1/about.html')
+    return render(request,'about/about.html')
 
 def signup(request):
     form = forms.SignupForm()
