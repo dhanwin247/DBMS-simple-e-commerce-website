fef92251d03054299a4ccc01206c52d6e0283e14
varun-raghavendra <varun.raghavendra1official@gmail.com>
2020-06-06 22:28:32 +0530

    add home and about

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,7 @@ from app1 import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('signup/',views.signup, name='signup'),
+    path('home/',views.home, name='home'),
     path('login/',views.login, name='login'),
     path('about/',views.about, name='about')
 ]
