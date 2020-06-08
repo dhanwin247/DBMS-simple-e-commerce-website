01d08f788ae991794fd2ffe88a26c3b5ebe10dca
dhanwin247 <dhanwinrao18@gmail.com>
2020-06-09 01:17:11 +0530

    Created products as an independent app

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('',views.ProductHomeView.as_view()),
-    path('list/',views.ProductListView.as_view(),name='list'),#some mistake
+    path('list/',views.ProductListView.as_view(),name='prodlist'),
+    path('list/<pk>/',views.ProductDetailView.as_view(),name='proddetail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from . import models
 # Create your views here.
 
 class ProductHomeView(TemplateView):
     template_name = 'products/product_base.html'
-    
+
 class ProductListView(ListView):
     model = models.Phone
+    template_name = 'products/product_list.html'
 
 class ProductDetailView(DetailView):
     context_object_name = 'product_details'
