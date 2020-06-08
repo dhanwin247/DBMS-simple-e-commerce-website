778aef230bd073ee6d009004f18528bf25cc768c
varun-raghavendra <varun.raghavendra1official@gmail.com>
2020-06-08 22:25:59 +0530

    fix bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,9 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('signup',views.signup, name='signup'),
-    path('home',views.home, name='home'),
-    path('user_login',views.user_login, name='user_login'),
+    path('signup/',views.signup, name='signup'),
+    path('home/',views.home, name='home'),
+    path('user_login/',views.user_login, name='user_login'),
     # path('logout/',views.user_logout,name='logout')
-    path('about',views.about, name='about'),
+    path('about/',views.about, name='about'),
 ]
